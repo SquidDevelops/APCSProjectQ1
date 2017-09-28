30097620a861faa94db7b6e58723f8de434048ae
SquidDevelops <zklgeorge56@gmail.com>
2017-09-28 08:05:03 -0400

    Made the frame atomic
    Signed-off-by: SquidDevelops <zklgeorge56@gmail.com>

--- src/net/tesqgr/VotingFrakenstien/Bug_Test/BugViewer.java
+++ src/net/tesqgr/VotingFrakenstien/Bug_Test/BugViewer.java
@@ -1,21 +1,20 @@
 package net.tesqgr.VotingFrakenstien.Bug_Test;
 
 import javax.swing.JFrame;
+import java.util.concurrent.atomic.AtomicReference;
 //import java.awt.*;
 
-public class BugViewer
-{
-    public static void main(String[] args)
-    {
-        JFrame frame = new JFrame();
+public class BugViewer {
+    public static void main(String[] args) {
+        AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
 
-        frame.setSize(300, 400);
-        frame.setTitle("Two Bugs");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.get().setSize(300, 400);
+        frame.get().setTitle("Two Bugs");
+        frame.get().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         BugTest component = new BugTest();
-        frame.add(component);
+        frame.get().add(component);
 
-        frame.setVisible(true);
+        frame.get().setVisible(true);
     }
 }
