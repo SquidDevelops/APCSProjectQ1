228426b4783c8db1c99e50abe186b4c2bf734531
JasonLaptop <jay.ram@verizon.net>
2017-09-28 08:17:43 -0400

    Working Setup of Car Drawing Example
    Signed-off-by: JasonLaptop <jay.ram@verizon.net>

--- src/net/tesqgr/VotingFrakenstien/Bug_Test/BugTest.java
+++ src/net/tesqgr/VotingFrakenstien/Bug_Test/BugTest.java
@@ -4,8 +4,10 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
 
-public class BugTest extends JComponent {
-    public void paintComponent(Graphics g) {
+public class BugTest extends JComponent
+{
+    public void paintComponent(Graphics g)
+    {
         Graphics2D g2 = (Graphics2D) g;
         Bug bug1 = new Bug(0, 0);
         int x = getWidth() - 60;
