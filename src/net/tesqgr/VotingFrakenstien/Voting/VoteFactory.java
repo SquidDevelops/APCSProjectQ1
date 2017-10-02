41cc920ac2a7e0eeb869fa91944fad900827ff3e
SquidDevelops <zklgeorge56@gmail.com>
2017-10-02 08:18:34 -0400

    Added an add votes funcrion
    Signed-off-by: SquidDevelops <zklgeorge56@gmail.com>

--- src/net/tesqgr/VotingFrakenstien/Voting/VoteFactory.java
+++ src/net/tesqgr/VotingFrakenstien/Voting/VoteFactory.java
@@ -10,4 +10,13 @@ public class VoteFactory {
         return new DemocratVote(voteAPI);
     }
 
+    public static void createVotes(int democratVotes, int republicanVotes, VoteAPI voteAPI){
+        for(int i = 0 ; i < democratVotes; i++){
+            createDemocratVote(voteAPI);
+        }
+        for(int i = 0 ; i < republicanVotes; i++){
+            createRepublicanVote(voteAPI);
+        }
+    }
+
 }
