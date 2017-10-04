cefb6d0fac928069c488ffd97f55e8b71f9cfb08
SquidDevelops <zklgeorge56@gmail.com>
2017-10-04 08:56:44 -0400

    tigers
    Signed-off-by: SquidDevelops <zklgeorge56@gmail.com>

--- src/net/tesqgr/VotingFrakenstien/VotingFrakenstien.java
+++ src/net/tesqgr/VotingFrakenstien/VotingFrakenstien.java
@@ -1,8 +1,17 @@
 package net.tesqgr.VotingFrakenstien;
 
+import javax.swing.*;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class VotingFrakenstien {
 
   public static void main(String[] args){
+    AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
+
+    frame.get().setSize(800, 500);
+    frame.get().setTitle("your mom");
+    frame.get().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
   }
 }
