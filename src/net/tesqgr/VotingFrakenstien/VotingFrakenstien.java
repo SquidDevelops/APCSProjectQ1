95525ee97533cb6e2cac91b7ca181188cfb85781
SquidDevelops <zklgeorge56@gmail.com>
2017-10-06 08:16:32 -0400

    did some button tiger
    Signed-off-by: SquidDevelops <zklgeorge56@gmail.com>

--- src/net/tesqgr/VotingFrakenstien/VotingFrakenstien.java
+++ src/net/tesqgr/VotingFrakenstien/VotingFrakenstien.java
@@ -1,17 +1,29 @@
 package net.tesqgr.VotingFrakenstien;
 
+import net.tesqgr.VotingFrakenstien.Voting.VoteAPI;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class VotingFrakenstien {
 
-  public static void main(String[] args){
-    AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
 
-    frame.get().setSize(800, 500);
-    frame.get().setTitle("your mom");
-    frame.get().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public static void main(String[] args) {
+        AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
+        JPanel jPanel = new JPanel();
+        VoteAPI voteapi = new VoteAPI(1, 1, "Jason Ramos", "Jason Unramos");
 
-
-  }
+        frame.get().setSize(800, 500);
+        frame.get().setTitle("your mom");
+        frame.get().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton jbutton = new JButton("your mum");
+        jbutton.setSize(40, 30);
+        jPanel.add(jbutton);
+        //jbutton.setLocation(100,100);
+        jbutton.addActionListener(e -> System.out.println("isss"));
+        frame.get().add(jPanel);
+        frame.get().setVisible(true);
+    }
 }
