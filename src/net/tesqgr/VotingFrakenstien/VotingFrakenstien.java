1b1c290750dac45715be25493cf39ecfef9d2ed2
SquidDevelops <zklgeorge56@gmail.com>
2017-10-10 08:58:38 -0400

    added tiger button
    Signed-off-by: SquidDevelops <zklgeorge56@gmail.com>

--- src/net/tesqgr/VotingFrakenstien/VotingFrakenstien.java
+++ src/net/tesqgr/VotingFrakenstien/VotingFrakenstien.java
@@ -1,8 +1,10 @@
 package net.tesqgr.VotingFrakenstien;
 
+import net.tesqgr.VotingFrakenstien.Components.DemocratButton;
 import net.tesqgr.VotingFrakenstien.Voting.VoteAPI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.concurrent.atomic.AtomicReference;
@@ -12,17 +14,14 @@ public class VotingFrakenstien {
 
     public static void main(String[] args) {
         AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
-        JPanel jPanel = new JPanel();
         VoteAPI voteapi = new VoteAPI(1, 1, "Jason Ramos", "Jason Unramos");
         frame.get().setSize(800, 500);
         frame.get().setTitle("your mom");
         frame.get().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton jbutton = new JButton("your mum");
-        jbutton.setSize(40, 30);
-        jPanel.add(jbutton);
-        //jbutton.setLocation(100,100);
-        jbutton.addActionListener(e -> System.out.println("isss"));
-        frame.get().add(jPanel);
+
+        frame.get().getContentPane().add(new DemocratButton("vote",  voteapi), BorderLayout.WEST);
+        frame.get().getContentPane().add(new DemocratButton("vote",  voteapi), BorderLayout.EAST);
+
         frame.get().setVisible(true);
     }
 }
