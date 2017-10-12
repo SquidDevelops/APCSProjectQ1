df683bc75a5c529f3a63cf9e5c5543ca02db735c
BuildTools <unconfigured@null.spigotmc.org>
2017-10-11 21:57:11 -0400

    Mere

--- src/net/tesqgr/VotingFrakenstien/VotingFrankenstien.java
+++ src/net/tesqgr/VotingFrakenstien/VotingFrankenstien.java
@@ -4,18 +4,20 @@ import net.tesqgr.VotingFrakenstien.Components.DemocratButton;
 import net.tesqgr.VotingFrakenstien.Components.RepublicanButton;
 import net.tesqgr.VotingFrakenstien.Components.VoteAnimals;
 import net.tesqgr.VotingFrakenstien.Voting.VoteAPI;
+import net.tesqgr.VotingFrakenstien.Voting.VoteUtils;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.concurrent.atomic.AtomicReference;
 //import java.awt.*;
 
-public class VotingFrankenstien
+public class VotingFrankenstien implements Runnable
 {
     public static VoteAPI voteAPI = new VoteAPI(1, 1, "George", "Ryan");
+    private static AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
 
     public static void main(String[] args) throws InterruptedException {
-        AtomicReference<JFrame> frame = new AtomicReference<>(new JFrame());
+
 
         VoteAnimals component = new VoteAnimals();
         frame.get().setSize(800, 800);
@@ -31,4 +33,12 @@ public class VotingFrankenstien
 
     }
 
+    @Override
+    public void run() {
+        if (VoteUtils.countDemocratVotes(voteAPI) >= 10) {
+            //TODO: Buisness logic for Democrat win goes here
+        } else if (VoteUtils.countRepublicanVotes(voteAPI) >= 10) {
+            //TODO: Buisness logic for republican win goes here
+        }
+    }
 }
